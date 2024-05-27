efccd41e64cba920ce9ec4a2178650563eaabe28
Minu-Ochieng <minuochieng@gmail.com>
2024-05-27 15:06:26 +0300

    first calculate in android

---
+++ app/src/main/java/com/example/calc/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.calc
+
+import android.media.tv.TvView
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var etNum1:EditText
+    lateinit var etNum2:EditText
+    lateinit var btnAdd:Button
+    lateinit var btnSubtract: Button
+    lateinit var btnMultiply:Button
+    lateinit var btnDivide:Button
+    lateinit var tvResult: TextView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        castViews()
+
+        btnAdd.setOnClickListener{
+            val num1 = etNum1.text.toString().toInt()
+            val num2 = etNum2.text.toString().toInt()
+            val sum = num1 + num2
+            tvResult.text = sum.toString()
+
+        }
+
+        btnSubtract.setOnClickListener{
+            val num1 = etNum1.text.toString().toInt()
+            val num2 = etNum2.text.toString().toInt()
+            val sum = num1 - num2
+            tvResult.text = sum.toString()
+        }
+        btnMultiply.setOnClickListener{
+            val num1 = etNum1.text.toString().toInt()
+            val num2 = etNum2.text.toString().toInt()
+            val sum = num1 * num2
+            tvResult.text = sum.toString()
+        }
+        btnDivide.setOnClickListener{
+            val num1 = etNum1.text.toString().toInt()
+            val num2 = etNum2.text.toString().toInt()
+            val sum = num1 / num2
+            tvResult.text = sum.toString()
+        }
+    }
+
+    fun castViews(){
+        etNum1 =findViewById(R.id.etNum1)
+        etNum2 =findViewById(R.id.etNum2)
+        btnAdd = findViewById(R.id.btnAdd)
+        btnSubtract = findViewById(R.id.btnSubtract)
+        btnMultiply =findViewById(R.id.btnMultiply)
+        btnDivide = findViewById(R.id.btnDivide)
+        tvResult= findViewById(R.id.tvResult)
+    }
+}
